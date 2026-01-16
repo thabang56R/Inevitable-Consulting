0e7dd5863203db9ce9592f29c78df21a2abb9f5f
thabang56R <thabang56@hotmail.com>
2026-01-16 16:40:44 +0200

    Update contactInfo file

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const ContactInfo = () => {
   return (
     <section className="contact-info">
-      <p>📍 362 Tirong Estate, Johannesburg, 2000</p>
-    & <p> 📍 4 Nautalis , Emdo Park , Polokwane, 0742</p>
+      <p>📍 362 Isibaha Close ,Tirong Estate, Johannesburg, 2000</p>
+    & <p> 📍 4 Nautalis, Emdo Park , Polokwane, 0742</p>
       <p>📞 Call: (071) 534 7554</p>
-      <p>✉️ info@inevitableconsulting.co.za</p>
+      <p>✉️ info@inevitableaccounting.co.za</p>
       
       
     </section>
